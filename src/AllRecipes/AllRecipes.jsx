5f38f824688a9c4e278701102b40825c7d8e28bf
San400ys <116559884+San400ys@users.noreply.github.com>
2024-02-22 04:31:45 +0700

    Добавлен InfiniteScroll

--- src/AllRecipes/AllRecipes.jsx
+++ src/AllRecipes/AllRecipes.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import SelectFavorite from "../FavoritesRecipes/SelectFavorite";
+import InfiniteScroll from 'react-infinite-scroller';
 import AllRecipesStyle from "../AllRecipes/AllRecipesStyle";
 import Search from "../Search/Search";
 import SearchStyle from "../Search/SearchStyle";
 import GridStyle from "./GridStyle";
 import CardStyle from "./CardStyle";
-import { Link } from "react-router-dom";
-import SelectFavorite from "../FavoritesRecipes/SelectFavorite";
+import Scroll from "../Scroll/Scroll";
 
 const AllRecipes = ({ recipes, favoriteRecipes, setFavoriteRecipes }) => {
     const { filteredRecipes, toggleFavorite, updateFilteredRecipes } = SelectFavorite(recipes, favoriteRecipes, setFavoriteRecipes);
+    const { visibleRecipes, hasMore, loadMoreRecipes } = Scroll(filteredRecipes);
 
     return (
         <AllRecipesStyle>
             <SearchStyle>
                 <Search setFilteredRecipes={updateFilteredRecipes}/>
             </SearchStyle>
-            <GridStyle>
-                {filteredRecipes.map(({ id, title, image, prep_time }) =>
-                    <CardStyle key={id}>
-                        <Link to={`${id}`}>
-                            <img src={image} alt={title}/>
-                            <h1>{title}</h1>
-                            <p>Время приготовления: {prep_time}</p>
-                        </Link>
-                        <button onClick={() => toggleFavorite(id)}>
-                            {favoriteRecipes.some((recipe) => recipe.id === id)
-                                ? "Удалить из избранного"
-                                : "Добавить в избранное"}
-                        </button>
-                    </CardStyle>
-                )}
-            </GridStyle>
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={loadMoreRecipes}
+                hasMore={hasMore}
+                loader={<div className="loader" key={0}></div>}
+            >
+                <GridStyle>
+                    {visibleRecipes.map(({ id, title, image, prep_time }) => (
+                        <CardStyle key={id}>
+                            <Link to={`${id}`}>
+                                <img src={image} alt={title}/>
+                                <h1>{title}</h1>
+                                <p>Время приготовления: {prep_time}</p>
+                            </Link>
+                            <button onClick={() => toggleFavorite(id)}>
+                                {favoriteRecipes.some((recipe) => recipe.id === id)
+                                    ? "Удалить из избранного"
+                                    : "Добавить в избранное"}
+                            </button>
+                        </CardStyle>
+                    ))}
+                </GridStyle>
+            </InfiniteScroll>
         </AllRecipesStyle>
     );
 };
